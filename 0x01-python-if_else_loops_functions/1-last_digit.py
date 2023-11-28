e3badf3d94d364f88c4691e27f66397038cff21c
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-11-28 12:50:43 +0200

    python

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = number
+if number < 0:
+    num = -number
+    n = num % 10
+if n > 5:
+    print(f"Last digit of {number:d} is {n:d} and is greater than 5")
+elif n == 0:
+    print(f"Last digit of {number:d} is {n:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {n:d} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for i in range(26):
+    if chr(i + 97) not in "qe":
+        print(chr(i + 97), end="")
