003ef7b45bd8c19144e55de36a0b714f43608fea
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-11-30 16:11:22 +0200

    python

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from printalpha import printalphabet
+printalphabet()
---
+++ 0x02-python-import_modules/printalpha.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def printalphabet():
+    for i in range(26):
+        print(chr(65 +i), end="")
+    print()
