55596e3ba2ba5d44f7673c47cd271bcd22427573
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-01 11:56:11 +0200

    fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,57 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * len_linked - returns length of linked list
+ *
+ * @head: linked list
+ *
+ * Return: length
+*/
 int len_linked(listint_t *head)
 {
-    int i;
-    for (i = 0; head != NULL; i++)
-        head = head->next;
-    return (i);
+	int i;
 
+	for (i = 0; head != NULL; i++)
+		head = head->next;
+	return (i);
 }
+
+/**
+ * is_palindrome - checks if linkedlist is palindrome
+ *
+ * @head: linked list
+ *
+ * Return: 1 if it is
+ * 0 otherwise
+*/
 
 int is_palindrome(listint_t **head)
 {
-    listint_t *current;
-    if (!head || !(*head))
-        return (1);
-    current = (*head);
+	listint_t *current;
+	int *arr;
+	int i, j;
 
-    int *arr;
-    arr = malloc(sizeof(int) * len_linked(*head));
-    int i = 0, j;
-    while (current != NULL)
-    {
-        arr[i] = current->n;
-        current = current->next;
-        i++;
-    }
+	if (!head || !(*head))
+		return (1);
+	current = (*head);
 
-    for (j = 0; j < i; j++)
-    {
-        if (arr[j] != arr[i -1 - j])
-            return (0);
-    }
-    return (1);
+	arr = malloc(sizeof(int) * len_linked(*head));
+	i = 0;
+	while (current != NULL)
+	{
+		arr[i] = current->n;
+		current = current->next;
+		i++;
+	}
 
+	for (j = 0; j < i; j++)
+	{
+		if (arr[j] != arr[i - 1 - j])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+	free(arr);
+	return (1);
 }
