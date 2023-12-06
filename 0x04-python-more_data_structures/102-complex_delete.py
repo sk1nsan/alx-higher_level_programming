0746962b5e8c2a40c7bea31394cac07da6e6cc19
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-06 18:25:05 +0200

    python

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if a_dictionary:
+        a = a_dictionary.copy()
+        [a_dictionary.pop(x) for x, y in a.items()  if y == value]
+    return a_dictionary
