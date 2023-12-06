50be7e76374d368cc97bf2128976b3325025ab11
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-06 17:28:07 +0200

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary:
         r = list(a_dictionary.values())[0]
-        for x, y in a_dictionary.items():
-            if y > r:
-                r = y
+        for x in a_dictionary.keys():
+            if a_dictionary[x] > r:
+                r = a_dictionary[x]
         return r
     return None
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (roman_string is str):
+    if (isinstance(roman_string, str)):
         r = 0
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for i in range(len(roman_string)):
