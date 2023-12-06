3cdc2125bc9742601b7c752baf251b68376aaa60
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-06 18:55:52 +0200

    fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    r = 0
     if (isinstance(roman_string, str)):
-        r = 0
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for i in range(len(roman_string)):
             if i == len(roman_string) - 1:
