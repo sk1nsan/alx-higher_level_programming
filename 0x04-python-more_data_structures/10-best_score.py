609aa76d8a8ba12a393890cf57e7e38df89fb8a9
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-06 18:53:27 +0200

    fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        r = 0
-        for x in a_dictionary.keys():
-            if a_dictionary[x] > r:
-                r = a_dictionary[x]
+        r = list(a_dictionary.keys())[0]
+        for x, y in a_dictionary.items():
+            if y > a_dictionary[r]:
+                r = x
         return r
