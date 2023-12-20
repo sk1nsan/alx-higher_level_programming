1e1a6a74fe1a763b9850e5ac719e13a3dfcd6349
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2023-12-20 02:53:28 +0200

    102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """class Square"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size**2
+    def __lt__(self, other):
+        if other.area() > self.area():
+            return True
+        return False
+    def __le__(self, other):
+        if other.area() >= self.area():
+            return True
+        return False
+    def __eq__(self, other):
+        if other.area() == self.area():
+            return True
+        return False
+    def __ne__(self, other):
+        if other.area() != self.area():
+            return True
+        return False
+    def __ge__(self, other):
+        if other.area() <= self.area():
+            return True
+        return False
+    def __gt__(self, other):
+        if other.area() < self.area():
+            return True
+        return False
