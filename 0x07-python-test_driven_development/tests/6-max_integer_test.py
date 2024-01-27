3f212b817ee0eb98e49adfd8804a15c8c84b049e
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-01-27 18:26:11 +0200

    style fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,8 +4,9 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     def test_list(self):
         self.assertEqual(max_integer(), None)
         self.assertEqual(max_integer([1]), 1)
-        self.assertEqual(max_integer([-2, 0, -100 , -1250 ,-999999]), 0)
+        self.assertEqual(max_integer([-2, 0, -100, -1250, -999999]), 0)
