8a69bbab0072b658f4ea7c8e2fae121d42636042
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 01:50:14 +0200

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Rectangle  """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self ,size):
+        """Square init function"""
+
+        self.integer_validator("size", size)
+        self.__size = size
+    def area(self):
+        """Square area function"""
+
+        return self.__size * self.__size
