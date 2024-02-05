f34e075f8015f593fbda41c07b31ebdec6b7e546
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 01:51:03 +0200

    10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,11 +6,12 @@ Rectangle = __import__("9-rectangle").Rectangle
 class Square(Rectangle):
     """Square class"""
 
-    def __init__(self ,size):
+    def __init__(self, size):
         """Square init function"""
 
         self.integer_validator("size", size)
         self.__size = size
+
     def area(self):
         """Square area function"""
 
