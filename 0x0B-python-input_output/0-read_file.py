21da1055bf491aa04a94bc89e2c824d6d3756f15
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 20:51:17 +0200

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,7 @@
 """read_file function """
 
 
-
 def read_file(filename=""):
     """function to read file"""
-    with open(filename, encoding="UTF8") as f:
+    with open(filename, "r", encoding="UTF8") as f:
         print(f.read())
