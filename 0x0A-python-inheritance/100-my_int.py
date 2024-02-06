33789c25c6d46bdbd797c64773af30f049025d78
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 02:11:20 +0200

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" MyInt  """
+
+
+class MyInt(int):
+    def __eq__(self, otherint):
+        """ Equality method  """
+        return int.__ne__(self, otherint)
+
+    def __ne__(self, otherint):
+        """ Inequality method  """
+        return int.__eq__(self, otherint)
