f9dfaec73769dcb967a036ee95c37a7e7dd15e09
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 02:50:50 +0200

    101

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,8 +4,6 @@
 
 def add_attribute(class_name, attribute_name, value):
     """add_attribute function"""
-    if attribute_name in globals():
-        raise TypeError("can't add new attribute")
     if class_name not in globals():
         raise TypeError("can't add new attribute")
     setattr(class_name, attribute_name, value)
