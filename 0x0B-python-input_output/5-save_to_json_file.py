31a17e2c1cbce18a7a136648aefa27b8b8abe92d
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-02-06 21:09:47 +0200

    5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""json module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """function that returns the JSON representation of an object (string)"""
+    with open(filename, "a", encoding="UTF8") as f:
+        return f.write(json.dumps(my_obj))
