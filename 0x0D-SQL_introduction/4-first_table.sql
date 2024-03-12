ec192f9d75215509c6aa094df2473d5f59eb969f
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-03-12 10:25:14 +0200

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---  lists all the tables of a database in the MySQL server.
+--  creates a table called first_table in the current database in your the server.
 CREATE TABLE first_table (id INT, name VARCHAR(256))
