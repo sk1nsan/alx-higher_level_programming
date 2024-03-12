fd498ec96a4caad68a949c231d41603505239813
sk1nsan <138858455+sk1nsan@users.noreply.github.com>
2024-03-12 11:40:26 +0200

    101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT city, AVG(value) as avg_temp from temperatures WHERE month in (7, 8) group by city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
